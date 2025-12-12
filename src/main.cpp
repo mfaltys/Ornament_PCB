7a2e29ed47a6e3f137645b1055bdfd4cbfbded48
= <mfaltys@gmail.com>
2025-12-12 13:50:51 -0600

    Add animation Update on time to 5 minutes

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,21 @@
 #include <avr/sleep.h>
 
 const uint8_t ledPins[] = {8, 0, 1, 7, 6, 2, 3, 5, 4};
+const uint8_t zigzagLine[] = {2, 6, 0, 4, 8, 5, 1, 7, 3};
+const unsigned int stepDelay = 74;
+const uint8_t tailLength = 3;
+
+volatile bool buttonPressed = false;
 
 ISR(PORTA_PORT_vect) {
-  // Clear PA2 interrupt flag
   PORTA.INTFLAGS = PIN2_bm;
+  buttonPressed = true;
 }
 
 void sleepNow() {
   set_sleep_mode(SLEEP_MODE_PWR_DOWN);
   cli();
   sleep_enable();
-  // Clear any pending interrupt flag before sleeping
   PORTA.INTFLAGS = PIN2_bm;
   sei();
   sleep_cpu();
@@ -20,11 +24,7 @@ void sleepNow() {
 }
 
 void setup() {
-  // Set unused pin PA3 (pin 10) to INPUT_PULLUP
   PORTA.PIN3CTRL = PORT_PULLUPEN_bm;
-
-  // Configure button pin PA2 (pin 9) for pullup and falling edge interrupt
-  // PA2 is asynchronous and can wake from PWR_DOWN with FALLING
   PORTA.PIN2CTRL = PORT_PULLUPEN_bm | PORT_ISC_FALLING_gc;
 
   for (uint8_t i = 0; i < sizeof(ledPins)/sizeof(ledPins[0]); i++) {
@@ -33,42 +33,90 @@ void setup() {
   }
 }
 
-void setLEDs(bool on) {
+void allLEDsOff() {
   for (uint8_t i = 0; i < sizeof(ledPins)/sizeof(ledPins[0]); i++) {
-    digitalWrite(ledPins[i], on ? HIGH : LOW);
+    digitalWrite(ledPins[i], LOW);
   }
 }
 
-void loop() {
-  static bool ledsOn = false;
-  static unsigned long timerStart = 0;
-
-  if (digitalRead(9) == LOW) {
-    delay(50); // debounce
+bool checkButton() {
+  if (buttonPressed) {
+    buttonPressed = false;
+    delay(50);
     if (digitalRead(9) == LOW) {
-      if (ledsOn) {
-        // toggle off
-        ledsOn = false;
-        setLEDs(false);
-      } else {
-        // turn on
-        ledsOn = true;
-        timerStart = millis();
-        setLEDs(true);
-      }
-      // wait for button release
       while (digitalRead(9) == LOW);
-      delay(50); // additional debounce
+      delay(50);
+      return true;
+    }
+  }
+  return false;
+}
+
+void loop() {
+  static bool animating = false;
+  static unsigned long animStart = 0;
+
+  if (checkButton()) {
+    if (animating) {
+      animating = false;
+      allLEDsOff();
+    } else {
+      animating = true;
+      animStart = millis();
     }
   }
 
-  if (ledsOn && millis() - timerStart >= 120000) {
-    ledsOn = false;
-    setLEDs(false);
-  }
+  if (animating) {
+    if (millis() - animStart >= 300000) {
+      animating = false;
+      allLEDsOff();
+    } else {
+      // Animation 1: Zigzag snake
+      int numLEDs = sizeof(zigzagLine)/sizeof(zigzagLine[0]);
+      for (int i = 0; i < numLEDs + tailLength && animating; i++) {
+        if (i < numLEDs) digitalWrite(zigzagLine[i], HIGH);
+        if (i >= tailLength) digitalWrite(zigzagLine[i - tailLength], LOW);
+        delay(stepDelay);
+        if (checkButton()) { animating = false; allLEDsOff(); break; }
+        if (millis() - animStart >= 300000) { animating = false; allLEDsOff(); break; }
+      }
+      
+      for (int i = numLEDs - 1 + tailLength; i >= 0 && animating; i--) {
+        if (i < numLEDs) digitalWrite(zigzagLine[i], HIGH);
+        if (i >= tailLength) digitalWrite(zigzagLine[i - tailLength], LOW);
+        delay(stepDelay);
+        if (checkButton()) { animating = false; allLEDsOff(); break; }
+        if (millis() - animStart >= 300000) { animating = false; allLEDsOff(); break; }
+      }
 
-  // If LEDs are off, go to sleep
-  if (!ledsOn) {
+      // Animation 2: Sequential sweep
+      for (uint8_t i = 0; i < sizeof(ledPins)/sizeof(ledPins[0]) && animating; i++) {
+        digitalWrite(ledPins[i], HIGH);
+        delay(stepDelay);
+        if (checkButton()) { animating = false; allLEDsOff(); break; }
+        if (millis() - animStart >= 300000) { animating = false; allLEDsOff(); break; }
+        digitalWrite(ledPins[i], LOW);
+      }
+      
+      for (int i = sizeof(ledPins)/sizeof(ledPins[0]) - 1; i >= 0 && animating; i--) {
+        digitalWrite(ledPins[i], HIGH);
+        delay(stepDelay);
+        if (checkButton()) { animating = false; allLEDsOff(); break; }
+        if (millis() - animStart >= 300000) { animating = false; allLEDsOff(); break; }
+        digitalWrite(ledPins[i], LOW);
+      }
+
+      // Animation 3: Random sparkle
+      for (uint8_t j = 0; j < 20 && animating; j++) {
+        uint8_t randLED = random(0, sizeof(ledPins)/sizeof(ledPins[0]));
+        digitalWrite(ledPins[randLED], HIGH);
+        delay(stepDelay);
+        if (checkButton()) { animating = false; allLEDsOff(); break; }
+        if (millis() - animStart >= 300000) { animating = false; allLEDsOff(); break; }
+        digitalWrite(ledPins[randLED], LOW);
+      }
+    }
+  } else {
     sleepNow();
   }
 }
